f4a30fdcd09aeec65341ff332ada01e458286e88
UsiasSG <160407522+UsiasSG@users.noreply.github.com>
2024-08-03 03:36:03 -0600

    Add files via upload

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author saqui
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        frm_calcu frm = new frm_calcu();
+              frm.show();
+              
+        // TODO code application logic here
+    }
+    
+}
---
+++ Calculadora/src/modelo/operacion.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+ 
+//import java.io.FileWriter;
+//import java.io.FileReader;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+
+
+
+//@override
+//public void agregar() {
+
+//guardarEnArchivo();
+
+//}
+
+//private void guardarEnArchivo(){
+  //   String datos = getNumero1 () 
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
